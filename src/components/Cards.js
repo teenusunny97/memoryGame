cb2343735d275d8054df27d81b51755a3304ff13
Teenu Sunny <teenusunny1997@gmail.com>
2024-06-25 18:05:35 +0530

    formatted

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,56 +1,54 @@
 import { useState } from "react"
 import Card from "./Card"
-const Cards=()=>{
-    const [items,setItems]=useState(
+const Cards = () => {
+    const [items, setItems] = useState(
         [
-            {id:"1",img:'/images/goa.jpg',stat:""},
-            {id:"1",img:'/images/goa.jpg',stat:""},
-            {id:"2",img:'/images/ladakh.jpg',stat:""},
-            {id:"2",img:'/images/ladakh.jpg',stat:""},
-            {id:"3",img:'/images/manali.jpg',stat:""},
-            {id:"3",img:'/images/manali.jpg',stat:""},
-            {id:"4",img:'/images/coorg.jpg',stat:""},
-            {id:"4",img:'/images/coorg.jpg',stat:""},
-            {id:"5",img:'/images/joker.jpg',stat:"active"},
-        ].sort(()=>Math.random()-0.5)
+            { id: "1", img: '/images/goa.jpg', stat: "" },
+            { id: "1", img: '/images/goa.jpg', stat: "" },
+            { id: "2", img: '/images/ladakh.jpg', stat: "" },
+            { id: "2", img: '/images/ladakh.jpg', stat: "" },
+            { id: "3", img: '/images/manali.jpg', stat: "" },
+            { id: "3", img: '/images/manali.jpg', stat: "" },
+            { id: "4", img: '/images/coorg.jpg', stat: "" },
+            { id: "4", img: '/images/coorg.jpg', stat: "" },
+            { id: "5", img: '/images/joker.jpg', stat: "active" },
+        ].sort(() => Math.random() - 0.5)
     )
-    const [prev,setPrev]=useState("")
-const handleClick=(current)=>{
-    console.log("clscurrent",current,"prev",prev)
-  
-    if(prev===""){
-        setPrev(current)
-        setItems([...items])
-        items[current].stat="active"
+    const [prev, setPrev] = useState("")
+    const handleClick = (current) => {
+
+        if (prev === "") {
+            setPrev(current)
+            setItems([...items])
+            items[current].stat = "active"
+        }
+        else {
+
+            if (items[prev].id === items[current].id) {
+                items[prev].stat = "correct"
+                items[current].stat = "correct"
+                setItems([...items])
+                setPrev("")
+            }
+            else {
+                items[prev].stat = "wrong"
+                items[current].stat = "wrong"
+                setItems([...items])
+                setTimeout(() => {
+                    items[prev].stat = ""
+                    items[current].stat = ""
+                    setPrev("")
+                }, 1000)
+            }
+
+        }
     }
-    else{
-       
-        if(items[prev].id===items[current].id){
-            items[prev].stat="correct"
-            items[current].stat="correct"
-            setItems([...items])
-            setPrev("")
-        }
-        else{
-            items[prev].stat="wrong"
-            items[current].stat="wrong"
-            setItems([...items])
-            setTimeout(()=>{
-               items[prev].stat=""
-            items[current].stat=""
-            setPrev("")
-            },1000)
-        }
-    
-}
-}
-    return(
+    return (
         <div className="container">
-            {console.log("items",items)}
-             { items.map((item,index)=>(
-                <Card key={index} index={index} item={item} handleClick={handleClick}/>
-             ))
-             }
+            {items.map((item, index) => (
+                <Card key={index} index={index} item={item} handleClick={handleClick} />
+            ))
+            }
         </div>
     )
 }
